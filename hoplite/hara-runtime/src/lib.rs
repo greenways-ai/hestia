da73e7b2871b3ab0bb316712f5b663ba9f531335
Chris Zheng <zcaudate@outlook.com>
2026-07-31 02:23:03 +1000

    fix Rust loading of metadata-rich HIR

--- hoplite/hara-runtime/src/lib.rs
+++ hoplite/hara-runtime/src/lib.rs
@@ -3767,6 +3767,29 @@ mod tests {
     }
 
     #[test]
+    fn definitions_accept_source_metadata_around_hir_syntax() {
+        let mut runtime = Runtime::new();
+        runtime
+            .eval_text(concat!(
+                "(defn wrapped ^{:line 1} [value] value)",
+                " (defn wrapped-many",
+                " ^{:line 2} ([value] value)",
+                " ^{:line 3} ([left right] (+ left right)))"
+            ))
+            .unwrap();
+        assert_eq!(
+            runtime
+                .eval_text(concat!(
+                    "[(wrapped 42)",
+                    " (wrapped-many 42)",
+                    " (wrapped-many 19 23)]"
+                ))
+                .unwrap(),
+            "[42 42 42]"
+        );
+    }
+
+    #[test]
     fn namespace_values_and_operations_match_java_registry_semantics() {
         let mut runtime = Runtime::new();
         let initial_namespace_count = runtime.namespace_registry.all().len();
