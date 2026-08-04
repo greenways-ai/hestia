89797b595f2fb4bf838ff88cfaf9e6a218129746
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-08-04 20:05:00 +1000

    Export document gateway client

--- browser/src/index.js
+++ browser/src/index.js
@@ -13,3 +13,4 @@ export * from "./agent-gateway.js";
 export * from "./agent-room-storage.js";
 export * from "./document-hcv1.js";
 export * from "./document-records.js";
+export * from "./document-gateway.js";
